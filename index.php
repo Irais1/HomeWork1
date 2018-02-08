fd02ed71354f25a0cdd3a14d4a7ac777e390f362
Irais Gopar <igopar-osorio@csumb.edu>
2018-02-08 08:27:43 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+        <title>Operating Systems</title>
+        
+        <link href ="CSS/style.css" rel =" stylesheet" type = "text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1 style="font-size:60px;">Operating Systems (OS)</h1>
+        </header>
+        <nav>
+            <hr width ="50%">
+            <a href ="index.php"><i>Home</i></a>
+            <a href = "Linux.html">Linux</a>
+            <a href = "Android.html">Android</a>
+            <a href = "Mac.html">Mac OS</a>
+        </nav>
+        <main>
+                <h1>What are Operating Systems?</h1><br />
+                <p>An operating system is the most important software on the computer. It deals with the momory and processes of the computer. It also manages the software and hardware. It lets the user communicate with the computer; if a computer does not contain an operating system, it will be completely useless.</p>
+                <h2>The job of an Os</h2>
+                <p>The OS manages all the software and hardware on the computer. More often then not, there are several computer programs running at once; all needing access your computer's central processing unit(CPU), memeory, and storage. The operating systems makes sure each program gets what is needed.</p>
+                <h2>Types of Operating Systems</h2>
+                <figure id = "fig1">
+                    <img class = "resize2" src="https://cdn-images-1.medium.com/max/1920/1*72cktuehSngJboCjBkOXew.jpeg"></img>
+                </figure>
+                <p>Users do not have to worry to much about installing operating systems as most computers come pre-loaded with them. Most people will just use the pre-installed OS; however, it is possible to upgrading and/or change operating systems.</p>
+                <h2>Most common operating systems:</h2>
+                <ul class = "center">
+                    <li>Microsoft Windows,</li>
+                    <li>Mac OS X,</li>
+                    <li>Linux</li>
+                </ul>
+                <p>For more information, please click <a href = "https://www.gcflearnfree.org/computerbasics/understanding-operating-systems/1/">here</a></p>
+                <br /> <br />
+                <!--<em>"With ordinary talent and extraordinary <strong>perserverance</strong>, all things attainable"</em><br />
+                -Thomas F. Buxton -->
+    
+            </div>
+        </main>
+        <!-- This is the footern -->
+        <!-- tHE FOOTER GOES INSIDE THE BODY BUT NOT ALWAYS-->
+        <footer id="fter">
+            <hr>
+            CST 336. 2018&copy; Gopar<br />
+            <strong>Disclaimer:</strong> The information in this webpage can fictitous.<br />
+            It is used for academic purposes only.<br />
+            <img src="https://csumb.edu/sites/default/files/styles/profile_large/public/photo_1_6.jpg?itok=N4I4g9Wl">
+        </footer>
+    </body>
+</html>
